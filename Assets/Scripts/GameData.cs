c889ad819a3d578a750ddabe3d17dc81e871491a
HWU110013 <110013@mail.hwu.edu.tw>
2025-12-03 15:18:24 +0800

    UI HPBarCtrl

--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameData
+public static class GameData
 {
-    
+    public static int hp;
+    public static int hpMax;
 
 }
