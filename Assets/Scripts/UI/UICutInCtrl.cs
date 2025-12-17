1a50964384cc88a86ec25fcf688cffbbbe54ac6b
HWU110013 <110013@mail.hwu.edu.tw>
2025-12-17 15:56:48 +0800

    UI_CutIn建置

---
+++ Assets/Scripts/UI/UICutInCtrl.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UICutInCtrl : MonoBehaviour
+{
+    public Animator animator;
+
+    public Text cutInText;
+    public string startInfo;
+    public string endInfo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
